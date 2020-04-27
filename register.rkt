7060806077402a8ed468e9b70ae9d2f8c56ec642
Metaxal <laurent.orseau@gmail.com>
2020-04-27 08:16:29 +0100

    add register message after setup

--- register.rkt
+++ register.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require quickscript/library
-         racket/runtime-path)
+         racket/runtime-path
+         (for-syntax racket/base))
 
 ;;; To register the script directory to use with quickscript
 ;;; run this file in DrRacket, or on the command line with
@@ -8,3 +9,9 @@
 
 (define-runtime-path script-dir "scripts")
 (add-third-party-script-directory! script-dir)
+
+(begin-for-syntax
+  ;; To be displayed after setup
+  (displayln "Register me as a quickscript directory (on first setup only) with:
+racket -l quickscript-extra/register"))
+
