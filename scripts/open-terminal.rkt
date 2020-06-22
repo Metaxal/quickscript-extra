521fa095e0521ffd91b12f6137b251b61936dd07
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2020-06-22 15:13:23 +0100

    add macOSX support

--- scripts/open-terminal.rkt
+++ scripts/open-terminal.rkt
@@ -3,17 +3,29 @@
          racket/path
          quickscript)
 
-(script-help-string "Open a terminal in the current directory (linux only).")
+(script-help-string "Open a terminal in the current directory.")
 
 (define-script open-terminal
-  #:label "Open terminal here"
+  #:label "Open terminal"
   #:menu-path ("&Utils")
-  #:os-types (unix)
-  (λ (str #:file f)  
-    (define dir (path->string (path-only f)))
-    (system (string-append "gnome-terminal"
-                           " --working-directory=\"" dir "\""
-                           " -t \"" dir "\""
-                           "&"))
-    #f))
+  #:os-types (unix macosx)
+  #:output-to message-box
+  
+  (cond
 
+    [(equal? (system-type 'os) 'unix)
+     (λ (str #:file f)  
+       (define dir (path->string (path-only f)))
+       (system (string-append "gnome-terminal"
+                              " --working-directory=\"" dir "\""
+                              " -t \"" dir "\""
+                              "&"))
+       #f)]
+    [(equal? (system-type 'os) 'macosx)
+     (λ (str #:file f)  
+       (define dir (path->string (path-only f)))
+       (define osascriptdir (string-append
+                             (path->string (find-system-path 'pref-dir))
+                             "quickscript/user-scripts/"))
+       (system (string-append "osascript -e 'tell app \"Terminal\" to do script \"cd \\\"" dir "\\\"\"'" ))
+       #f)]))
