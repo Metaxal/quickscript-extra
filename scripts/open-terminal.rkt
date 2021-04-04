cefe55ece00c61e4e762cdc8a012aace76ad42a4
Metaxal <laurent.orseau@gmail.com>
2021-04-04 14:02:38 +0100

    Open terminal in current directory if file is false. Fix #19

--- scripts/open-terminal.rkt
+++ scripts/open-terminal.rkt
@@ -3,30 +3,25 @@
          racket/path
          quickscript)
 
-(script-help-string "Open a terminal in the current directory.")
+(script-help-string "Open a terminal in the directory of the current file.")
 
 (define-script open-terminal
   #:label "Open terminal here"
   #:menu-path ("&Utils")
   #:os-types (unix macosx windows)
-  #:output-to message-box
-
-  (case (system-type 'os)
-    [(unix)
-     (λ (str #:file f)  
-       (define dir (path->string (path-only f)))
+  (λ (str #:file f)
+    (unless f
+      (set! f (current-directory)))
+    (define dir (path->string (path-only f)))
+    (case (system-type 'os)
+      [(unix)
        (system (string-append "gnome-terminal"
                               " --working-directory=\"" dir "\""
                               " -t \"" dir "\""
-                              "&"))
-       #f)]
-    [(macosx)
-     (λ (str #:file f)  
-       (define dir (path->string (path-only f)))
+                              "&"))]
+      [(macosx)
        (system
-        (string-append "osascript -e 'tell app \"Terminal\" to do script \"cd \\\"" dir "\\\"\"'" ))
-       #f)]
-    [(windows)
-     (λ (str #:file f)
-       (define dir (path->string (path-only f)))
-       (shell-execute #f "cmd.exe" "" dir 'sw_shownormal))]))
+        (string-append "osascript -e 'tell app \"Terminal\" to do script \"cd \\\"" dir "\\\"\"'" ))]
+      [(windows)
+       (shell-execute #f "cmd.exe" "" dir 'sw_shownormal)])
+    #false))
