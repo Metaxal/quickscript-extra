f69aa014b4c1c305beb65748c135f9229ce180bd
Metaxal <laurent.orseau@gmail.com>
2021-08-01 15:20:03 +0100

    fix reverse lengths

--- scripts/indent-table.rkt
+++ scripts/indent-table.rkt
@@ -48,16 +48,17 @@ empty columns are added at the end of the shortest rows.
   (define-values (llines2 lens)
     (for/fold ([llines2 '()]
                [rev-lens '()]
-               #:result (values (reverse llines2) rev-lens))
+               #:result (values (reverse llines2) (reverse rev-lens)))
               ([l (in-list lines)])
       (define items (regexp-split px-splitter (string-trim l)))
-      ; Re-prepend the leading spaces to the first item to preserve indentation.
-      ; items cannot be empty.
-      (if (and (string=? "" (first items))
-               (empty? (rest items)))
+      (if (equal? items '(""))
+        ; Whitespace line, return an empty line
         (values (cons '() llines2) rev-lens)
+        ; Re-prepend the leading spaces to the first item to preserve indentation.
+        ; items cannot be empty.
         (let ([items (cons (string-append (first (regexp-match #px"^ *" l)) (first items))
                            (rest items))])
+          #;(pretty-print items)
       
           (define diff-n-items (- (length items) (length rev-lens)))
           (define new-rev-lens
@@ -65,17 +66,19 @@ empty columns are added at the end of the shortest rows.
                  (append (make-list (max 0 diff-n-items) 0)
                          rev-lens)
                  (append (make-list (max 0 (- diff-n-items)) 0)
-                         (map string-length items))))
+                         (reverse (map string-length items)))))
           (values (cons items llines2)
                   new-rev-lens)))))
 
   (string-join
    (for/list ([items (in-list llines2)])
-     (string-join
-      (for/list ([item (in-list items)]
-                 [len (in-list lens)])
-       (string-pad-right item len))
-      new-sep))
+     (string-trim
+      (string-join
+       (for/list ([item (in-list items)]
+                  [len (in-list lens)])
+         (string-pad-right item len))
+       new-sep)
+      #:left? #f))
    "\n"))
 
 (define-script indent-table
@@ -127,8 +130,8 @@ empty columns are added at the end of the shortest rows.
 
 (module+ test
   (require rackunit)
-  (define from
-    "\
+  (let ()
+    (define from "\
   (define immutable-string (string->immutable-string string))
   (define start (string-replacement-start replacement))
   (define end (string-replacement-original-end replacement))
@@ -137,18 +140,47 @@ empty columns are added at the end of the shortest rows.
   (define required-length (string-replacement-required-length replacement))
   (define original-length (string-length immutable-string))
 ")
-  (define to
-    "\
-  (define immutable-string (string->immutable-string string)) 
-  (define start            (string-replacement-start           replacement))                                                   
-  (define end              (string-replacement-original-end    replacement))                                                   
-  (define new-end          (string-replacement-new-end         replacement))                                                   
-  (define contents         (string-replacement-contents        replacement))                                                   
-  (define required-length  (string-replacement-required-length replacement))                                                   
-  (define original-length  (string-length immutable-string))  
+    (define to "\
+  (define immutable-string (string->immutable-string string))
+  (define start            (string-replacement-start           replacement))
+  (define end              (string-replacement-original-end    replacement))
+  (define new-end          (string-replacement-new-end         replacement))
+  (define contents         (string-replacement-contents        replacement))
+  (define required-length  (string-replacement-required-length replacement))
+  (define original-length  (string-length immutable-string))
 ")
-  (define str2 (indent-table* from #:sep " ("))
-  (define str3 (indent-table* str2 #:sep " rep"))
-  (check-equal? str3 to)
+    (define str2 (indent-table* from #:sep " ("))
+    (define str3 (indent-table* str2 #:sep " rep"))
+
+    #;(displayln str3)
+    (check-equal? str3 to))
+
+  (let ()
+    (define from "\
+  [union-into-string-replacement (reducer/c string-replacement? string-replacement?)]
+  [string-replacement-render (-> string-replacement? string? immutable-string?)]
+  [string-apply-replacement (-> string? string-replacement? immutable-string?)]
+  [file-apply-string-replacement! (-> path-string? string-replacement? void?)]
+  [inserted-string? predicate/c]
+  [inserted-string (-> string? inserted-string?)]
+  [inserted-string-contents (-> inserted-string? immutable-string?)]
+  [copied-string? predicate/c]
+")
+    (define to
+      "\
+  [union-into-string-replacement  (reducer/c   string-replacement? string-replacement?)]
+  [string-replacement-render      (->          string-replacement? string?               immutable-string?)]
+  [string-apply-replacement       (->          string?             string-replacement?   immutable-string?)]
+  [file-apply-string-replacement! (->          path-string?        string-replacement?   void?)]
+  [inserted-string?               predicate/c]
+  [inserted-string                (->          string?             inserted-string?)]
+  [inserted-string-contents       (->          inserted-string?    immutable-string?)]
+  [copied-string?                 predicate/c]
+")
+    (define str2 (indent-table* from #:sep " "))
+    #;(displayln str2)
+    (check-equal? str2 to))
+
+  
   )
 
